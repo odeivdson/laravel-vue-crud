571ee2410823e11d1af8e03d8481bfb5131b2fca
Deivdson Oliveira <odeivdson@gmail.com>
2019-09-17 12:38:23 -0300

    Initial commit

---
+++ resources/js/components/CreateComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Criar um Post</h1>
+    <form @submit.prevent="addPost">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Título do Post:</label>
+            <input type="text" class="form-control" v-model="post.title">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Corpo do Post:</label>
+              <textarea class="form-control" v-model="post.body" rows="5"></textarea>
+            </div>
+          </div>
+        </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Criar</button>
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+        return {
+          post:{}
+        }
+    },
+    methods: {
+      addPost(){
+        let uri = '/api/post/create';
+        this.axios.post(uri, this.post).then((response) => {
+          this.$router.push({name: 'posts'});
+        });
+      }
+    }
+  }
+</script>
---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row justify-content-center">
+      <div class="col-md-8">
+          <div class="card card-default">
+              <div class="card-header">Componente Home</div>
+
+              <div class="card-body">
+                  Aqui vai o conteúdo do componente Home.
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Componente criado com sucesso.')
+        }
+    }
+</script>
